8dae02520b39627f8556e85ea4ee68d0c1f96f13
ccaianMC <97574761+ccaianMC@users.noreply.github.com>
2025-06-30 20:09:35 +0800

    Update 2 - Added game mode for picture - Added more bird species - Improvements to UI

---
+++ src/BirdData.jsx
@@ -0,0 +1,228 @@
+
+export const birdData = [
+    {
+      id: 1,
+      name: "Slaty-breasted Rail",
+      audioUrl: "audio/XC735649 - Slaty-breasted Rail - Lewinia striata gularis.mp3",
+      pictureUrl: "https://i0.wp.com/singaporebirds.com/wp-content/uploads/2016/01/slaty-breasted-rail-fyap9650-103eos1d-110701.jpg?w=400&h=267&fit=crop",
+      rarity: "uncommon",
+      hints: ["Lewinia striata gularis"]
+    },
+    {
+      id: 2,
+      name: "Spotted Dove",
+      audioUrl: "audio/XC938779 - Spotted Dove - Spilopelia chinensis.mp3",
+      pictureUrl: "https://i0.wp.com/singaporebirds.com/wp-content/uploads/2016/01/spotted-dove-141221-fy7d3190-101eos7d.jpg?w=317&h=211&ssl=1&fit=crop",
+      rarity: "very common",
+      hints: ["Spilopelia chinensis"]
+    },
+    {
+      id: 3,
+      name: "Plaintive Cuckoo",
+      pictureUrl: "https://i0.wp.com/singaporebirds.com/wp-content/uploads/2016/01/plaintive-cuckoo-130808-109eos1d-fy1x6546.jpg?w=962&h=641&ssl=1&fit=crop",
+      audioUrl: "audio/XC240755 - Plaintive Cuckoo - Cacomantis merulinus.mp3",
+      rarity: "common",
+      hints: ["Cacomantis merulinus"]
+    },
+    {
+      id: 4,
+      name: "Banded Bay Cuckoo",
+      audioUrl: "audio/XC645001 - Banded Bay Cuckoo - Cacomantis sonneratii.mp3",
+      pictureUrl: "https://i0.wp.com/singaporebirds.com/wp-content/uploads/2016/01/banded-bay-cuckoo-140115-112eos1d-fy1x9213.jpg?fit=1200%2C800&ssl=1",
+      rarity: "common",
+      hints: ["Cacomantis sonneratii"]
+    },
+    {
+      id: 5,
+      name: "Asian Koel",
+      rarity: "very common",
+      pictureUrl: "https://i0.wp.com/singaporebirds.com/wp-content/uploads/2021/07/asian-koel-170129-109eos1d-f1x24860-n.jpg?w=408&h=272&ssl=1&fit=crop",
+      audioUrl: "audio/XC175830 - Asian Koel - Eudynamys scolopaceus.mp3",
+      hints: ["Eudynamys scolopaceus"]
+    },
+    {
+      id: 6,
+      name: "Ruddy-breasted Crake",
+      rarity: "uncommon",
+      pictureUrl: "https://i0.wp.com/singaporebirds.com/wp-content/uploads/2016/01/ruddy-breasted-crake-fy1x9116-102eos1d-121021.jpg?w=641&h=427&ssl=1&fit=crop",
+      audioUrl: "audio/XC352048 - Ruddy-breasted Crake - Zapornia fusca.mp3",
+      hints: ["Zapornia fusca"]
+    },
+    {
+      id: 7,
+      name: "Red-legged Crake",
+      rarity: "common",
+      pictureUrl: "https://i0.wp.com/singaporebirds.com/wp-content/uploads/2016/01/red-legged-crake-fyap3264-111eos1d-120520.jpg?w=400&h=267&fit=crop",
+      audioUrl: "audio/XC685889 - Red-legged Crake - Rallina fasciata.mp3",
+      hints: ["Rallina fasciata"]
+    },
+    {
+      id: 8,
+      name: "White-breasted Waterhen",
+      rarity: "very common",
+      pictureUrl: "https://i0.wp.com/singaporebirds.com/wp-content/uploads/2016/01/white-breasted-waterhen-fy1x2148-113eos1d-140124.jpg?w=640&h=427&ssl=1&fit=crop",
+      audioUrl: "audio/XC432691 - White-breasted Waterhen - Amaurornis phoenicurus.mp3",
+      hints: ["Amaurornis phoenicurus"]
+    },
+    {
+      id: 9,
+      name: "Pacific Golden Plover",
+      rarity: "migrants",
+      pictureUrl: "https://i0.wp.com/singaporebirds.com/wp-content/uploads/2024/04/Pacific-Golden-Plover-161024-106EOS1D-F1X22771-W.jpg?w=1500&ssl=1&fit=crop",
+      audioUrl: "audio/XC762049 - Pacific Golden Plover - Pluvialis fulva.wav",
+      hints: ["Pluvialis fulva"]
+    },
+    {
+      id: 10,
+      name: "Oriental Pied Hornbill",
+      rarity: "common",
+      pictureUrl: "https://i0.wp.com/singaporebirds.com/wp-content/uploads/2016/01/oriental-pied-hornbill-fy1x6080-121eos1d-150619.jpg",
+      audioUrl: "audio/XC829411 - Oriental Pied Hornbill - Anthracoceros albirostris.mp3",
+      hints: ["Anthracoceros albirostris"]
+    },
+    {
+      id: 11,
+      name: "Pin-striped Tit-Babbler",
+      rarity: "very common",
+      pictureUrl: "https://i0.wp.com/singaporebirds.com/wp-content/uploads/2016/01/pin-striped-tit-babbler-img_8069-109eos7d-111114.jpg?w=641&h=427&ssl=1&fit=crop",
+      audioUrl: "audio/XC937886 - Pin-striped Tit-Babbler - Mixornis gularis gularis.mp3",
+      hints: ["Mixornis gularis gularis"]
+    },
+    {
+      id: 12,
+      name: "Oriental Magpie-Robin",
+      rarity: "common",
+      pictureUrl: "https://i0.wp.com/singaporebirds.com/wp-content/uploads/2016/01/oriental-magpie-robin-fyap6041-101eos1d-110401.jpg?fit=1200%2C800&ssl=1&fit=crop",
+      audioUrl: "audio/Oriental Magpie Robin Calling & Close up - Punakha, Bhutan.mp3",
+      hints: ["Copsychus saularis"]
+    },
+    {
+      id: 13,
+      name: "Yellow-vented Bulbul",
+      rarity: "very common",
+      pictureUrl: "https://i0.wp.com/singaporebirds.com/wp-content/uploads/2016/01/yellow-vented-bulbul-110331-101eos1d-fyap5460.jpg?fit=1200%2C800&ssl=1&fit=crop",
+      audioUrl: "audio/XC723609 - Yellow-vented Bulbul - Pycnonotus goiavier.mp3",
+      hints: ["Pycnonotus goiavier"]
+    },
+    {
+      id: 14,
+      name: "Changeable Hawk-Eagle",
+      rarity: "common",
+      pictureUrl: "https://i0.wp.com/singaporebirds.com/wp-content/uploads/2016/01/changeable-hawk-eagle-fy1x2395-104eos1d-121202.jpg?w=408&h=272&ssl=1&fit=crop",
+      audioUrl: "audio/XC507284 - Changeable Hawk-Eagle - Nisaetus cirrhatus.mp3",
+      hints: ["Nisaetus cirrhatus"]
+    },
+    {
+      id: 15,
+      name: "Large-tailed Nightjar",
+      audioUrl: "audio/XC828236 - Large-tailed Nightjar - Caprimulgus macrurus bimaculatus.mp3",
+      pictureUrl: "https://i0.wp.com/singaporebirds.com/wp-content/uploads/2016/01/large-tailed-nightjar-fy7d0875-102eos7d-150315.jpg?fit=1200%2C800&ssl=1&fit=crop",
+      rarity: "very common",
+      hints: ["Caprimulgus macrurus bimaculatus"]
+    },
+    {
+      id: 16,
+      name: "Savanna Nightjar",
+      pictureUrl: "https://i0.wp.com/singaporebirds.com/wp-content/uploads/2021/07/savanna-nightjar-190828-119eos1d-f1x24814-n.jpg?w=317&h=212&ssl=1&fit=crop",
+      audioUrl: "audio/XC58142 - Savanna Nightjar - Caprimulgus affinis affinis.mp3",
+      rarity: "uncommon",
+      hints: ["Caprimulgus affinis affinis"]
+    },
+    {
+      id: 17,
+      name: "Stork-billed Kingfisher",
+      audioUrl: "audio/XC482361 - Stork-billed Kingfisher - Pelargopsis capensis.mp3",
+      pictureUrl: "https://i0.wp.com/singaporebirds.com/wp-content/uploads/2021/07/stork-billed-kingfisher-200928-101canon-fy5r8525-n.jpg?fit=1500%2C1000&ssl=1&fit=crop",
+      rarity: "common",
+      hints: ["Pelargopsis capensis"]
+    },
+    {
+      id: 18,
+      name: "Blue-rumped Parrot",
+      rarity: "uncommon",
+      pictureUrl: "https://i0.wp.com/singaporebirds.com/wp-content/uploads/2016/01/blue-rumped-parrot-fy1x9286-115eos1d-140425.jpg?fit=1200%2C800&ssl=1&fit=crop",
+      audioUrl: "audio/XC642692 - Blue-rumped Parrot - Psittinus cyanurus.mp3",
+      hints: ["Psittinus cyanurus"]
+    },
+    {
+      id: 19,
+      name: "Long-tailed Parakeet",
+      rarity: "very common",
+      pictureUrl: "https://i0.wp.com/singaporebirds.com/wp-content/uploads/2021/07/long-tailed-parakeet-190705-118nd500-fyp_1390-n.jpg?fit=1500%2C1000&ssl=1&fit=crop",
+      audioUrl: "audio/XC662677 - Long-tailed Parakeet - Psittacula longicauda.mp3",
+      hints: ["Psittacula longicauda"]
+    },
+    {
+      id: 20,
+      name: "Rose-ringed Parakeet",
+      rarity: "common",
+      pictureUrl: "https://i0.wp.com/singaporebirds.com/wp-content/uploads/2021/07/rose-ringed-parakeet-210316-104msdcf-fry09012-n.jpg?fit=1500%2C1000&ssl=1&fit=crop",
+      audioUrl: "audio/XC837321 - Rose-ringed Parakeet - Psittacula krameri.mp3",
+      hints: ["Psittacula krameri"]
+    },
+    {
+      id: 21,
+      name: "Collared Kingfisher",
+      rarity: "very common",
+      pictureUrl: "https://i0.wp.com/singaporebirds.com/wp-content/uploads/2021/09/CollaredKingfisher_KeitaSin.jpg?w=1500&ssl=1&fit=crop",
+      audioUrl: "audio/XC978467 - Collared Kingfisher - Todiramphus chloris.mp3",
+      hints: ["Todiramphus chloris"]
+    },
+    {
+      id: 22,
+      name: "Olive-winged Bulbul",
+      rarity: "common",
+      pictureUrl: "https://i0.wp.com/singaporebirds.com/wp-content/uploads/2016/01/olive-winged-bulbul-fyap9734-110eos1d-120504.jpg?fit=1200%2C800&ssl=1&fit=crop",
+      audioUrl: "audio/XC175831 - Olive-winged Bulbul - Pycnonotus plumosus plumosus.mp3",
+      hints: ["Pycnonotus plumosus plumosus"]
+    },
+    {
+      id: 23,
+      name: "Straw-headed Bulbul",
+      rarity: "common",
+      pictureUrl: "https://i0.wp.com/singaporebirds.com/wp-content/uploads/2021/07/straw-headed-bulbul-200411-115msdcf-fyp03940-n.jpg?fit=1500%2C1000&ssl=1",
+      audioUrl: "audio/Straw-headed Bulbul singing in the wild Singapore Birds.mp3",
+      hints: ["Pycnonotus zeylanicus"]
+    },
+    {
+      id: 24,
+      name: "Common Tailorbird",
+      rarity: "very common",
+      pictureUrl: "https://i0.wp.com/singaporebirds.com/wp-content/uploads/2018/06/common-tailorbird-110615-103eos1d-fyap7321.jpg",
+      audioUrl: "audio/XC644999 - Common Tailorbird - Orthotomus sutorius.mp3",
+      hints: ["Orthotomus sutorius"]
+    },
+    {
+      id: 25,
+      name: "Ashy Tailorbird",
+      rarity: "common",
+      pictureUrl: "https://i0.wp.com/singaporebirds.com/wp-content/uploads/2016/01/ashy-tailorbird-fyap9632-111eos1d-120627.jpg?w=479&h=320&ssl=1",
+      audioUrl: "audio/XC820397 - Ashy Tailorbird - Orthotomus ruficeps.mp3",
+      hints: ["Orthotomus ruficeps"]
+    },
+    {
+      id: 26,
+      name: "Greater Racket-tailed Drongo",
+      rarity: "common",
+      pictureUrl: "https://i0.wp.com/singaporebirds.com/wp-content/uploads/2021/07/greater-racket-tailed-drongo-201001-102canon-fy5r2237-n.jpg?fit=1500%2C1000&ssl=1&fit=crop",
+      audioUrl: "audio/XC978463 - Greater Racket-tailed Drongo - Dicrurus paradiseus.mp3",
+      hints: ["Dicrurus paradiseus"]
+    },
+    {
+      id: 27,
+      name: "Lesser Green Leafbird",
+      rarity: "rare",
+      pictureUrl: "https://i0.wp.com/singaporebirds.com/wp-content/uploads/2018/03/lesser-green-leafbird-170517-100nd500-fyp_25191.jpg?fit=1200%2C800&ssl=1&fit=crop",
+      audioUrl: "audio/XC971349 - Lesser Green Leafbird - Chloropsis cyanopogon cyanopogon.mp3",
+      hints: ["Chloropsis cyanopogon cyanopogon"]
+    },
+    {
+      id: 28,
+      name: "Mangrove Blue Flycatcher",
+      rarity: "rare",
+      pictureUrl: "https://i0.wp.com/singaporebirds.com/wp-content/uploads/2022/12/Mangrove-Blue-Flycatcher-220327-143MSDCF-FRY07178-N.jpg?w=1500&ssl=1&fit=crop",
+      audioUrl: "audio/XC895306 - Mangrove Blue Flycatcher - Cyornis rufigastra rufigastra.mp3",
+      hints: ["Cyornis rufigastra rufigastra"]
+    }
+];
+
